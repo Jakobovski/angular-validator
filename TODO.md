309224dbd15191c4b929873646e6b993a9b261ce
Zohar Jackson <zohar.jackson+github@gmail.com>
2015-11-11 18:17:36 +0200

    Begin work on v2. Made form name optional

---
+++ TODO.md
@@ -0,0 +1,23 @@
+## TODO
+
+### Version 2.0
+- update readme docs
+- mention a break in compatibility with version 1.x
+- remove dependency on $scope.form.model
+- add global form validation
+    - add area to display error
+- change src back to dist in demo
+- add names to elements that are missing names (inputs and form)
+- are the function in the linking function global?
+- should we split link into controller and link
+- update the example and remove 'form' from the models
+
+### Features
+- custom message containers
+- add some standard validation types
+    - ssn
+    - USA phone
+    - email-standard
+    - password-weak
+    - password-med
+    - password-strong
--- demo/app.js
+++ demo/app.js
@@ -3,45 +3,42 @@ angular.module('angular-validator-demo', ['angularValidator']);
 
 angular.module('angular-validator-demo').controller('DemoCtrl', function($scope) {
 
-	$scope.submitMyForm = function() {
-		alert("Form submitted");
-	};
+    $scope.submitMyForm = function() {
+        alert("Form submitted");
+    };
 
 
-	$scope.myCustomValidator = function(text) {
-		return true;
-	};
+    $scope.myCustomValidator = function(text) {
+        return true;
+    };
 
 
-	$scope.anotherCustomValidator = function(text) {
-		if (text === "rainbow") {
-			return true;
-		} else return "type in 'rainbow'";
-	};
+    $scope.anotherCustomValidator = function(text) {
+        if (text === "rainbow") {
+            return true;
+        } else return "type in 'rainbow'";
+    };
 
 
-	$scope.passwordValidator = function(password) {
+    $scope.passwordValidator = function(password) {
 
-		if (!password) {
-			return;
-		}
-		else if (password.length < 6) {
-			return "Password must be at least " + 6 + " characters long";
-		}
-		else if (!password.match(/[A-Z]/)) {
-			return "Password must have at least one capital letter";
-		}
-		else if (!password.match(/[0-9]/)) {
-			return "Password must have at least one number";
-		}
+        if (!password) {
+            return;
+        } else if (password.length < 6) {
+            return "Password must be at least " + 6 + " characters long";
+        } else if (!password.match(/[A-Z]/)) {
+            return "Password must have at least one capital letter";
+        } else if (!password.match(/[0-9]/)) {
+            return "Password must have at least one number";
+        }
 
-		return true;
-	};
-}).factory('customMessage', function () {
+        return true;
+    };
+}).factory('customMessage', function() {
     // invalid message service with message function
     return {
         // scopeElementModel is the object in scope version, element is the object in DOM version
-        message: function (scopeElementModel, element) {
+        message: function(scopeElementModel, element) {
             var errors = scopeElementModel.$error;
             if (errors.maxlength) {
                 // be careful with the quote
